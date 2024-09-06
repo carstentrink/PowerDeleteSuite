d538ed416a6d4762c48645d56668b93b707aca45
Boobs <86918103+braboobssiere@users.noreply.github.com>
2024-09-06 10:23:28 +0700

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,31 +1,29 @@
 javascript: (function() {
-  window.bookmarkver = "1.4";
-  var isReddit =
-    document.location.hostname.split(".").slice(-2).join(".") === "reddit.com";
-  var isOverview = document.location.href.match(/\/user\b/);
+  window.bookmarkver = '1.4';
+  var isReddit = document.location.hostname.split('.').slice(-2).join('.') === 'reddit.com';
+  var isOverview = !!document.location.href.match(/\/overview\b/i);
+
   if (isReddit && isOverview) {
-    var cachBustUrl = `?${new Date().getDate()}`;
-    var cachBustUrl = 'https://raw.githubusercontent.com/braboobssiere/PowerDeleteSuite_copy/master/powerdeletesuite.js?' + (new Date().getDate());
-    fetch(cachBustUrl)
+    var cacheBustUrl = 'https://raw.githubusercontent.com/j0be/PowerDeleteSuite/master/powerdeletesuite.js?' + (new Date().getDate());
+
+    fetch(cacheBustUrl)
       .then(function(response) {
         return response.text();
       })
       .then(function(data) {
-        var script = document.createElement("script");
-        script.id = "pd-script";
+        var script = document.createElement('script');
+        script.id = 'pd-script';
         script.innerHTML = data;
-        document.getElementsByTagName("head")[0].appendChild(script);
+        document.getElementsByTagName('head')[0].appendChild(script);
       })
       .catch(function() {
-        alert("Error retrieving PowerDeleteSuite from github");
+        alert('Error retrieving PowerDeleteSuite from GitHub');
       });
-  } else if (
-    confirm(
-      "This script can only be run from your own user profile on reddit. Would you like to go there now?"
-    )
-  ) {
-    document.location = "https://old.reddit.com/u/me/overview";
+  } else if (confirm('This script can only be run from your own user profile on Reddit. Would you like to go there now?')) {
+    document.location = 'https://old.reddit.com/u/me/overview';
   } else {
-    alert("Please go to your reddit profile before running this script");
+    alert('Please go to your Reddit profile before running this script');
   }
 })();
+
+
